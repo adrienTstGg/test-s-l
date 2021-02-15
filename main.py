bf61377f8d0f43e8f0d41081141837610429cf2e
adrienTstGg <79060877+adrienTstGg@users.noreply.github.com>
2021-02-15 22:41:51 +0100

    Create main.py
    tst

---
+++ main.py
@@ -0,0 +1,8 @@
+#hardcoded api key
+API_KEY_1=SsLkJCTKTzyDPNnG7Ge4vSxGoha9GJ9mZXZcs4xY62pFKM6Jg598nepY9nirJiJJ
+
+#hardcoded api key2
+API_KEY_2=95jMzZk2hCeeH77yPgqGoZBuFrR4V7wQDbidBhNVeJ3cVjMBXEuapSngjRoHq9y6
+
+#Commented API key
+API_KEY_3=Rmbozof6QXeUH73hYaADDxj3CVqKKBfpcVTZKWxNZiAT8X4yKfda3BBuNYCYBfZa
